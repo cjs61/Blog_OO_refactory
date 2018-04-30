14c7fdf55c6443614df883f46e29b2e5ee86db85
Unknown <barre.js@gmail.com>
2018-04-30 13:12:56 +0200

    Création du dossier et de la class config
    config(2) est simplement supprimé car il existait déjà
    
    ici j'obtiens le db_user

--- app/App.php
+++ app/App.php
@@ -2,29 +2,18 @@
 namespace App;
 
 class App{
-	const DB_NAME = 'blog';
-	const DB_USER = 'root';
-	const DB_PASS = '';
-	const DB_HOST = 'localhost';
+
 
 	private static $database;
-	private static $title = 'Blog photo';
+	public $title = "Blog photo";
+	private static $_instance;
 	
 	
-public static function getDb(){
-	if( self::$database === null ){
-		self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
-	}
-	return self::$database;
-	}
-	public static function notFound(){
-		header("HTTP/1.0 404 not found");
-	header("location:index.php?p=404");
-	}
-	public static function getTitle(){
-		return self::$title;
-	}
-	public static function setTitle($title){
-		self::$title = $title . ' | ' . self::$title;
+	public static function getInstance(){
+		if(is_null(self::$_instance)){
+			self::$_instance = new App();
+
+		}
+				return self::$_intance;
 	}
 }
--- config/config.php
+++ config/config.php
@@ -1,19 +1,7 @@
 <?php
-use App\App;
-use App\Table\Categorie;
-use App\Table\Article;
-
-
-$post = Article::find($_GET['id']);
-if($post === false){
-	App::notFound();
-}
-App::setTitle($post->titre);
-$categorie = Categorie::find($post->categories_id);
-?>
-
-<h1><?= $post->titre; ?></h1>
-
-<p><em><?= $categorie->titre; ?></em></p>
-
-<p><?= $post->contenu; ?></p>
+return   array(
+	"db_user" => "root", 
+	"db_pass" => "",
+	"db_host" => "localhost",
+	"db_name" => "blog"
+);
